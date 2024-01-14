8259ba728faed163ffe193c4b21e3e4ea021791e
W1ngzy <48943742+W1ngzy@users.noreply.github.com>
2024-01-14 18:51:27 +0200

    Main logic and round loop implementaion

--- js/main.js
+++ js/main.js
@@ -5,34 +5,55 @@ function getComputerChoice() {
   if (choice === 2) return "Scissors";
 }
 
-function playRound(playerChoice, computerChoice) {
-  switch (playerChoice) {
-    case "Paper":
-      switch (computerChoice) {
-        case "Rock":
-          return "Paper beats rock, player wins!";
-        case "Scissors":
-          return "Scissors beats paper, computer wins!";
-        case "paper":
-          return "It's a tie!";
-      }
-    case "Rock":
-      switch (computerChoice) {
-        case "Rock":
-          return "It's a tie!";
-        case "Paper":
-          return "Paper beats rock, computer wins!";
-        case "Scissors":
-          return "Rock beats scissors, player wins!";
-      }
-    case "Scissors":
-      switch (computerChoice) {
-        case "Rock":
-          return "Rock beats scissors, computer wins!";
-        case "Paper":
-          return "Scissors beats paper, player wins!";
-        case "Scissors":
-          return "It's a tie!";
-      }
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  function playRound() {
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    const playerChoice = prompt("Enter: 'Rock', 'Paper' or 'Scissors'").toLocaleLowerCase();
+    const computerChoice = getComputerChoice();
+    switch (playerChoice) {
+      case "paper":
+        switch (computerChoice) {
+          case "Rock":
+            playerScore++;
+            return "Paper beats rock, player wins!";
+          case "Scissors":
+            computerScore++;
+            return "Scissors beats paper, computer wins!";
+          case "paper":
+            return "It's a tie!";
+        }
+      case "rock":
+        switch (computerChoice) {
+          case "Rock":
+            return "It's a tie!";
+          case "Paper":
+            computerScore++;
+            return "Paper beats rock, computer wins!";
+          case "Scissors":
+            playerScore++;
+            return "Rock beats scissors, player wins!";
+        }
+      case "scissors":
+        switch (computerChoice) {
+          case "Rock":
+            computerScore++;
+            return "Rock beats scissors, computer wins!";
+          case "Paper":
+            playerScore++;
+            return "Scissors beats paper, player wins!";
+          case "Scissors":
+            return "It's a tie!";
+        }
+    }
   }
+  while (playerScore !== 3 && computerScore !== 3) {
+    console.log(playRound());
+  }
+  if (playerScore > computerScore) alert("You win");
+  alert("Computer wins");
 }
+
+game();
